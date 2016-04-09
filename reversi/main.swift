990776784a8583f3d90c8e6a0d0893e1c1223176
kiteretsu-elekitel <kiteretsu.elekitel@gmail.com>
2016-04-09 15:26:29 +0900

    Initial Commit

---
+++ reversi/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  reversi
+//
+//  Created by Darjeeling on 2016/04/09.
+//  Copyright © 2016年 Darjeeling. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
